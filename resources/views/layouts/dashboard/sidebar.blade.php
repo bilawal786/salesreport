21a521acda5b95431313c00ffeda86f7f9fb3a36
Sonu <bilawal.jabbar.786@gmail.com>
2022-03-29 17:54:08 +0500

    new code 1.1

--- resources/views/layouts/dashboard/sidebar.blade.php
+++ resources/views/layouts/dashboard/sidebar.blade.php
@@ -2,7 +2,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{route('index')}}" class="brand-link">
-        <img src="{{asset($sitelogo)}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+        <img src="{{asset($sitelogo)}}" alt="AdminLTE Logo" class="brand-image elevation-3"
              style="opacity: .8">
         <span class="brand-text font-weight-light">{{ $siteTitle }}</span>
     </a>
@@ -15,9 +15,9 @@
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 @if(!empty($user->image))
-                    <img src="{{asset($user->image)}}" class="img-circle elevation-2" alt="User Image">
+                    <img src="{{asset($user->image)}}" class="elevation-2" alt="User Image">
                 @else
-                    <img src="{{asset('assets/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
+                    <img src="{{asset('assets/dist/img/user2-160x160.jpg')}}" class="elevation-2"
                          alt="User Image">
                 @endif
             </div>
